52f607e11247e11a8c352974642e287a05d355e7
YamaTatsu10969 <52.wadachi.96@gmail.com>
2020-01-16 16:13:25 +0900

    add comments

--- FinalProject/GsTodo/GsTodo/TaskListPage/TaskListViewController.swift
+++ FinalProject/GsTodo/GsTodo/TaskListPage/TaskListViewController.swift
@@ -14,21 +14,34 @@ class TaskListViewController: UIViewController, UITableViewDelegate, UITableView
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // この ViewController で delegate のメソッドを使うために記述している。
         tableView.delegate = self
+        // この ViewController で datasouce のメソッドを使うために記述している。
         tableView.dataSource = self
+        // nib と xib はほぼ一緒
+        let nib = UINib(nibName: "CustomCell", bundle: nil)
+        // tableView に使う xib ファイルを登録している。
+        tableView.register( nib, forCellReuseIdentifier: "CustomCell")
+        
+        // デリゲートのメソッドを使うために記述している。
         TaskCollection.shared.delegate = self
+        
         setupNavigationBar()
         // Do any additional setup after loading the view.
     }
     
     // MARK: UITableView
+    
+    /// 1つの Section の中の Row　の数を定義する(セルの数を定義)
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return TaskCollection.shared.tasks.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
-        cell.textLabel?.text = TaskCollection.shared.tasks[indexPath.row].title
+        // 登録したセルを使う。 as! CustomCell としないと、UITableViewCell のままでしか使えない。
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath) as! CustomCell
+        cell.titleLabel?.text = TaskCollection.shared.tasks[indexPath.row].title
         return cell
     }
     
@@ -47,6 +60,7 @@ class TaskListViewController: UIViewController, UITableViewDelegate, UITableView
 extension TaskListViewController: TaskCollectionDelegate {
     // デリゲートのメソッド
     func reload() {
+        // tableView をリロードして、画面を更新する。
         tableView.reloadData()
     }
 }
